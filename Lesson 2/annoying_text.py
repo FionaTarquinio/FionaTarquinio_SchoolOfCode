5a9cd32f06b9c4bf74c72b6fee20ebc045bda9e0
FionaTarquinio <158007592+FionaTarquinio@users.noreply.github.com>
2024-02-03 19:16:36 +1100

    Uploaded Lesson 2 assignment

---
+++ Lesson 2/annoying_text.py
@@ -0,0 +1,6 @@
+original_text = "yabba dabba doo"
+shouting_text = original_text.upper()
+annoying_text = (shouting_text + " ")*9 + shouting_text
+print(f"My annoying text is \"{annoying_text}\".")
+print(f"It has {len(annoying_text)} characters.")
+
