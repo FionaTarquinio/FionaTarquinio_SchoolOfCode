3c746dc1b44ea10d5541e4bb3f9766061e284379
INTERNAL\ft18 <fiona.tarquinio@delwp.vic.gov.au>
2024-02-20 21:23:25 +1100

    Added if/else statement

--- Lesson_4/palindrome.py
+++ Lesson_4/palindrome.py
@@ -2,4 +2,8 @@ input_word = input("Enter your word:\n")
 list_letters = list(input_word.lower())
 list_letters.reverse()
 input_word_reversed = "".join(list_letters)
-print(f"Is word a palindrome?\n{input_word.lower() == input_word_reversed}")
+is_palindrome = input_word.lower() == input_word_reversed
+if is_palindrome == True:
+   print(f"Your word, '{input_word}', is a palindrome\n")
+else:
+   print(f"Your word, '{input_word}', is not a palindrome\n")
