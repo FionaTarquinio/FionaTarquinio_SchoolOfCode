bc1ed354f14a81dcdb34da36f1650eb4643cea06
INTERNAL\ft18 <fiona.tarquinio@delwp.vic.gov.au>
2024-03-28 15:47:06 +1100

    Added .lower() to adjust for case

--- Lesson_9/coffee_and_code.py
+++ Lesson_9/coffee_and_code.py
@@ -14,22 +14,22 @@ class SmartCoffeeMachine(KitchenAppliance):
         self.coffee_menu = coffee_menu
 
     def update_menu(self, coffee_type):
-        if coffee_type not in self.coffee_menu:
-            self.coffee_menu.append(coffee_type)
-            print(f"'{coffee_type}' has been added to the menu of your {self.brand} {self.model_number}.\n")
+        if coffee_type.lower() not in self.coffee_menu:
+            self.coffee_menu.append(coffee_type.lower())
+            print(f"'{coffee_type.lower()}' has been added to the menu of your {self.brand} {self.model_number}.\n")
         else:
-            print(f"'{coffee_type}' is already on the menu of your {self.brand} {self.model_number}.\n")
+            print(f"'{coffee_type.lower()}' is already on the menu of your {self.brand} {self.model_number}.\n")
         
     def make_coffee(self, coffee_type):
-        while (coffee_type not in self.coffee_menu):
-            print(f"'{coffee_type}' is not on the menu of your {self.brand} {self.model_number}.\nWould you like to add it to the menu? (y/n):\n")
+        while (coffee_type.lower() not in self.coffee_menu):
+            print(f"'{coffee_type.lower()}' is not on the menu of your {self.brand} {self.model_number}.\nWould you like to add it to the menu? (y/n):\n")
             if input().lower() == "y":
-                my_machine.update_menu(coffee_type)
+                my_machine.update_menu(coffee_type.lower())
             
             print("Please select your coffee from the following menu options:")  
             for item in self.coffee_menu:
                 print(item)
-            coffee_type = input()    
+            coffee_type = input().lower()    
         
         print(f"Now making your {coffee_type}...")    
 
