54924df2992e9943bbad9e93cbcf8b9761c13027
INTERNAL\ft18 <fiona.tarquinio@delwp.vic.gov.au>
2024-04-09 22:20:13 +1000

    updates

--- Project/birthday_reminders.py
+++ Project/birthday_reminders.py
@@ -8,40 +8,48 @@ print(f"The date today is {today}, which is day {today_day_in_year} of {today.ye
 class Birthday(date):
     def __init__(self, year, month, day):
         super().__init__()
-
-    def calc_birthday_day_in_current_year(self):
-        birthday_date_this_year = date(today.year, self.month, self.day) 
-        birthday_day_in_current_year = int(birthday_date_this_year.strftime("%j"))
-        return birthday_day_in_current_year
-
-    def calc_age_now(self):
-        if self.calc_birthday_day_in_current_year() > today_day_in_year:
-            age_now = today.year - self.year - 1
-        else:
-            age_now = today.year - self.year
-        return age_now
-
-#    def calc_days_to_birthday(self):
-#        days_to_birthday = 
-
-#    def add_new_birthday(self, year, month, day):
+        
+    def birthday_in_current_year(self): # returns date of birthday in current year
+        return date(today.year, self.month, self.day) 
     
+    def birthday_in_next_year(self): # returns date of birthday in next year
+        return date(today.year + 1, self.month, self.day)
+    
+    def next_birthday(self): # returns date of next birthday
+        if today <= self.birthday_in_current_year():
+            return self.birthday_in_current_year()
+        else:
+            return self.birthday_in_next_year()
+    
+    def days_to_next_birthday(self): # calculates number of days to next birthday from today
+        number_of_days = self.next_birthday() - today
+        return number_of_days.days
 
+    def age_next_birthday(self):
+        if self.birthday_in_current_year() > today:
+            return today.year - self.year
+        else:
+            return today.year - self.year + 1
+        
+# Instantiate Birthday objects:
 
 jesse = Birthday(1976, 9, 23)
+fiona = Birthday(1980, 5, 26)
+owen = Birthday(2009, 6, 29)
+leon = Birthday(2010, 10, 27)
+henry = Birthday(2012, 11, 14)
 
-print(jesse.calc_birthday_day_in_current_year())
-print(jesse.calc_age_now())
+birthday_dictionary = {
+    "Jesse":jesse,
+    "Fiona":fiona,
+    "Owen":owen,
+    "Leon":leon,
+    "Henry":henry
+}
 
+birthday_list = [jesse, fiona, owen, leon, henry]
 
+for birthday in birthday_list:
+    a = birthday.days_to_next_birthday
+    print(a)
 
-#owen = Birthday(2008, 6, 29)
-#leon = Birthday(2010, 10, 27)
-#henry = Birthday(2012, 11, 14)
-
-
-
-
-# Make list of people
-# Function to define new birthday via user input and add to list
-# Use for loop to evaluate days to birthday for each person in list and return person with fewest days to birthday and their age
